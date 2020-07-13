99a7c595dcc4da639d1a66462277b89d3cb76aa8
TechyCriswiz <67975057+TechyCriswiz@users.noreply.github.com>
2020-07-13 03:20:01 -0500

    Add files via upload

---
+++ Strings.playground/Pages/Exercise-Go! Fight! Win!.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+/*:
+ ## Exercise: Go! Fight! Win!
+ 
+ Many schools have “fight songs”: Students learn at least some portion of the words and then sing the songs together loudly at school events like sports games.
+ 
+ You’ve decided that your school’s fight song needs a rewrite. You want to edit the song in code so you don’t have to copy and paste as much while you work.
+ 
+ 1. Edit the `song` to have more than a repeated refrain.
+ 2. Edit the `refrain` to have actual words.
+ 3. Edit the `refrain` to use the `schoolName` at least twice.
+ 4. Test your work by changing the school name to a fictional school.
+ 
+ Use string interpolation in case you decide to sell your brilliant song to another school.
+ 
+ - note:
+ Use the show result button to view the results of your work.
+ */
+
+
+let schoolName = "Kennedy Crusaders"
+let refrain = "You Don't Stand A Chance Against Our \(schoolName) \n You Are Gonna Lose, So All We'll Say Is See You Later"
+let extraRefrain = "Hoo Raw, Hoo Raw, Hoo Raw"
+
+let song = "\(refrain)\n\(refrain)\nYes, \(refrain) \n \(extraRefrain)"
+
+
+
+//:
+//:[Previous](@previous)  |  page 15 of 16 |  [Next: Exercise: Displaying Values](@next)
